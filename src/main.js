631180a4c6c1ab1be54cd3962a050039deb768b6
Lout33 <lyupanqui99@gmail.com>
2022-08-19 22:18:12 -0500

    part8:  -add 3d assets  -add instrution  -hide helpers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls} from '../threejs/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../threejs/examples/jsm/loaders/GLTFLoader.js';
 import GUI from '../threejs/examples/jsm/libs/lil-gui.module.min.js';
 
 import * as CANNON from "../cannonjs/cannon-es.js";
@@ -18,7 +19,7 @@ let cubeThree;
 let keyboard = {};
 
 // camera follow player
-let enableFollow;
+let enableFollow = true;
 
 // cannon variables
 let world;
@@ -31,7 +32,7 @@ let obstaclesBodies = [];
 let obstaclesMeshes = [];
 init();
 
-function init() {
+async function init() {
 
   // Scene
 	scene = new THREE.Scene();
@@ -54,10 +55,17 @@ function init() {
 	renderer.shadowMap.enabled = true;
 	renderer.outputEncoding = THREE.sRGBEncoding;
 
+  const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+  light.position.set( 1, 10, 6);
+  scene.add(light);
+
 
   // axesHelper
-	axesHelper = new THREE.AxesHelper( 100 );
-	scene.add( axesHelper );
+	// axesHelper = new THREE.AxesHelper( 100 );
+	// scene.add( axesHelper );
 
   // orbitControls
   controls = new OrbitControls(camera, renderer.domElement);
@@ -67,17 +75,19 @@ function init() {
   controls.dampingFactor = 0.2
   controls.minDistance = 10
   controls.maxDistance = 500
-  controls.enabled = true
+  controls.enabled = false
 
 	elThreejs.appendChild(renderer.domElement);
 
   initCannon();
 
+  addBackground();
+
   addPlaneBody();
   addPlane();
 
   addCubeBody();
-  addCube();
+  await addCube();
 
   addObstacleBody();
   addObstacle();
@@ -101,7 +111,9 @@ function animate(){
 	cannonDebugger.update();
 
   cubeThree.position.copy(cubeBody.position);
+  cubeThree.position.y = cubeBody.position.y - 1.3;
   cubeThree.quaternion.copy(cubeBody.quaternion);
+
   for (let i = 0; i < obstaclesBodies.length; i++) {
     obstaclesMeshes[i].position.copy(obstaclesBodies[i].position);
 		obstaclesMeshes[i].quaternion.copy(obstaclesBodies[i].quaternion);
@@ -112,32 +124,38 @@ function animate(){
 }
 
 function addCubeBody(){
-
-
-  let cubeShape = new CANNON.Box(new CANNON.Vec3(1,1,1));
+  let cubeShape = new CANNON.Box(new CANNON.Vec3(1,1.3,2));
   slipperyMaterial = new CANNON.Material('slippery');
   cubeBody = new CANNON.Body({ mass: 100,material: slipperyMaterial });
-  cubeBody.addShape(cubeShape);
+  cubeBody.addShape(cubeShape, new CANNON.Vec3(0,0,-1));
 
   const polyhedronShape = createCustomShape()
-  cubeBody.addShape(polyhedronShape, new CANNON.Vec3(-1, -1, 1));
+  cubeBody.addShape(polyhedronShape, new CANNON.Vec3(-1, -1.3, 1));
 
   // change rotation
   cubeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), Math.PI / 180 * 180);
   
   cubeBody.position.set(0, 2, 0);
 
+  cubeBody.linearDamping = 0.5;
+
   world.addBody(cubeBody);
-  console.log(cubeBody, "cubeBody");
 }
 
-function addCube(){
-  let geometry = new THREE.BoxGeometry(2,2,2);
-  let material = new THREE.MeshBasicMaterial({color: 'pink'});
-  cubeThree = new THREE.Mesh(geometry, material);
-  cubeThree.position.set(0, 1, 0);
-  console.log(cubeThree, "cube");
+async function addCube(){
+  // let geometry = new THREE.BoxGeometry(2,2,2);
+  // let material = new THREE.MeshBasicMaterial({color: 'pink'});
+  // cubeThree = new THREE.Mesh(geometry, material);
+  // cubeThree.position.set(0, 1, 0);
+  // console.log(cubeThree, "cube");
+  // scene.add(cubeThree);
+
+  const gltfLoader = new GLTFLoader().setPath( 'src/assets/' );
+	const carLoaddedd = await gltfLoader.loadAsync( 'car.glb' );
+
+	cubeThree = carLoaddedd.scene.children[0];
   scene.add(cubeThree);
+
 }
 
 
@@ -153,8 +171,10 @@ function addPlaneBody(){
 
 
 function addPlane(){
+  const texture = new THREE.TextureLoader().load( "src/assets/plane.png" );
+
   let geometry =  new THREE.BoxGeometry(20, 0, 200);
-  let material = new THREE.MeshBasicMaterial({color: 'gray'});
+  let material = new THREE.MeshBasicMaterial({map: texture});
   let planeThree = new THREE.Mesh(geometry, material);
   planeThree.position.set(0, 0, -90);
   scene.add(planeThree);
@@ -163,7 +183,7 @@ function addPlane(){
 function addObstacleBody(){
 
   for (let i = 0; i < 5; i++) {
-    let obstacleShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
+    let obstacleShape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
     obstacleBody = new CANNON.Body({ mass: 1 });
     obstacleBody.addShape(obstacleShape);
 		obstacleBody.position.set(0, 5,-(i+1) * 15);
@@ -176,8 +196,11 @@ function addObstacleBody(){
 
 function addObstacle(){
  
-  let geometry = new THREE.BoxGeometry(1,1,1);
-  let material = new THREE.MeshBasicMaterial({color: 'red'});
+  let geometry = new THREE.BoxGeometry(2,2,2);
+  const texture = new THREE.TextureLoader().load( "src/assets/obstacle.png" );
+
+  let material = new THREE.MeshBasicMaterial({ map: texture});
+
   let obstacle = new THREE.Mesh(geometry, material);
 
   for (let i = 0; i < 5; i++) {
@@ -190,8 +213,8 @@ function addObstacle(){
 
 function addContactMaterials(){
   const slippery_ground = new CANNON.ContactMaterial(groundMaterial, slipperyMaterial, {
-    friction: 0.0001,
-    restitution: 0.3,
+    friction: 0.00,
+    restitution: 0.1, //bounciness
     contactEquationStiffness: 1e8,
     contactEquationRelaxation: 3,
   })
@@ -216,22 +239,24 @@ function movePlayer(){
   // up letter W
   // if(keyboard[87]) cubeThree.position.z -= 0.1
   // if(keyboard[87]) cubeThree.translateZ(-0.1);
+
   const strengthWS = 500;
   const forceForward = new CANNON.Vec3(0, 0, strengthWS)
   if(keyboard[87]) cubeBody.applyLocalForce(forceForward);
 
-  // // down letter S
+  // down letter S
   const forceBack = new CANNON.Vec3(0, 0, -strengthWS)
   if(keyboard[83]) cubeBody.applyLocalForce(forceBack);
 
   // left letter A
   // if(keyboard[65]) cube.rotation.y += 0.01;
   // if(keyboard[65]) cube.rotateY(0.01);
+
   const strengthAD = 200;
   const forceLeft= new CANNON.Vec3(0, strengthAD, 0)
   if(keyboard[65]) cubeBody.applyTorque(forceLeft);
 
-  // // right letter D
+  // right letter D
   const forceRigth= new CANNON.Vec3(0, -strengthAD, 0)
   if(keyboard[68]) cubeBody.applyTorque(forceRigth);
 
@@ -247,9 +272,8 @@ function followPlayer(){
 
 function addGUI(){
   gui = new GUI();
-
   const options = {
-		orbitsControls: true
+		orbitsControls: false
 	}
 
   gui.add(options, 'orbitsControls').onChange( value => {
@@ -261,6 +285,20 @@ function addGUI(){
 			enableFollow = true;
 		}
 	});
+  gui.hide();
+
+
+  // show and hide GUI if user press g
+  window.addEventListener('keydown', function(event){
+    if(event.keyCode == 71){
+      if(gui._hidden){
+        gui.show();
+      }else{
+        gui.hide();
+      }
+    }
+  })
+
 
 }
 
@@ -275,6 +313,7 @@ function initCannon() {
 function initCannonDebugger(){
   cannonDebugger = new CannonDebugger(scene, world, {
 		onInit(body, mesh) {
+      mesh.visible = false;
 			// Toggle visibiliy on "d" press
 			document.addEventListener("keydown", (event) => {
 				if (event.key === "f") {
@@ -298,11 +337,29 @@ function createCustomShape(){
 	return new CANNON.ConvexPolyhedron({
 		vertices,
 		faces: [
-      [3, 4, 5], 
-			[2, 1, 0], 
-			[1,2,5,4], 
-			[0,3,4,1], 
-			[0,2,5,3], 
+      [3, 4, 5],
+			[2, 1, 0],
+			[1,2,5,4],
+			[0,3,4,1],
+			[0,2,5,3],
 		]
 	})
+}
+
+async function addBackground(){
+	const gltfLoader = new GLTFLoader().setPath( 'src/assets/' );
+
+	const mountainLoaded = await gltfLoader.loadAsync( 'mountain.glb' );
+	let mountainMesh = mountainLoaded.scene.children[0];
+	mountainMesh.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), -Math.PI / 180 *90);
+	mountainMesh.position.set(0, 60, -90);
+	mountainMesh.scale.set(0.008,0.008,0.008);
+	scene.add(mountainMesh);
+
+	const domeLoaded = await gltfLoader.loadAsync( 'skydome.glb' );
+	let domeMesh = domeLoaded.scene.children[0];
+	domeMesh.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), -Math.PI / 180 *90);
+	domeMesh.position.set(0, -40, 0);
+	domeMesh.scale.set(0.1, 0.1, 0.1);
+	scene.add(domeMesh);
 }
